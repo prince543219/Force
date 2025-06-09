d968ab78f30dca4719b26aac51ce7908cede691e
prince543219 <surajitsingh54321@gmail.com>
2025-06-09 03:33:56 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import re
 from os import environ
 
-API_ID = int(environ.get("API_ID", 123456))  # Replace with your own or set via env
-API_HASH = environ.get("API_HASH", "your_api_hash")
-BOT_TOKEN = environ.get("BOT_TOKEN", "your_bot_token")
+API_ID = 28716246
+API_HASH = "d9277abd08e0277e0a899415916e39b3"
+BOT_TOKEN = "7094072498:AAF7mDbdRRbSpYII7wMDjuh8J_0EAD5bK_U"
 
 id_pattern = re.compile(r'^-100\d+$')  # Only allow Telegram channel IDs
 
@@ -11,3 +11,4 @@ AUTH_CHANNEL = [
     int(ch) if id_pattern.match(ch) else ch
     for ch in environ.get("AUTH_CHANNEL", "").split()
 ]
+
